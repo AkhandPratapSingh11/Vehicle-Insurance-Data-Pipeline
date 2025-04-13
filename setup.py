75e5cb1e77ff475ea595a6e32aa38adc8f854e79
Akhand <singhakhandpratap136@gmail.com>
2025-04-13 09:33:16 +0530

    setup, pyproject and requirement file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akhand Pratap Singh",
+    author_email="singhakhandpratap136@gmail.com",
+    packages=find_packages()
+)
